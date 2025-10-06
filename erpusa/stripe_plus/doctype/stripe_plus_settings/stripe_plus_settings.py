e6e05d79063f80bc5899f8915f2bcaaeeaa8555b
utopiafallen <utopiafallen@gmail.com>
2025-10-06 14:02:42 -0700

    WIP Security Hardening Pt.2
    Not sure what the right way is to add a new custom field to Payment Requests as part of Stripe Plus so just manually editing the json files for now. The idea is to generate and store a random access code that will be included in the request URL in the future to prevent payment URLs from easily guessed.

--- erpusa/stripe_plus/doctype/stripe_plus_settings/stripe_plus_settings.py
+++ erpusa/stripe_plus/doctype/stripe_plus_settings/stripe_plus_settings.py
@@ -164,6 +164,7 @@ def validate_stripe_plus_fields(payment_request, method=None):
 
     if payment_request.docstatus == 2:
       payment_request.stripe_intent_id = None
+      payment_request.stripe_plus_access_token = secrets.token_urlsafe()
 
   else:
     return
