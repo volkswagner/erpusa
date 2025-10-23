9ad4c20dca1f4c0d5350906b10ad238514ba9042
markguchi <rmbg2798@gmail.com>
2025-10-23 11:55:24 -0400

    Added Tools in Subscription

--- erpusa/public/js/subscription.js
+++ erpusa/public/js/subscription.js
@@ -1,3 +1,9 @@
+const stripe_logo = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stripe" viewBox="0 0 16 16">
+        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.226 5.385c-.584 0-.937.164-.937.593 0 .468.607.674 1.36.93 1.228.415 2.844.963 2.851 2.993C11.5 11.868 9.924 13 7.63 13a7.7 7.7 0 0 1-3.009-.626V9.758c.926.506 2.095.88 3.01.88.617 0 1.058-.165 1.058-.671 0-.518-.658-.755-1.453-1.041C6.026 8.49 4.5 7.94 4.5 6.11 4.5 4.165 5.988 3 8.226 3a7.3 7.3 0 0 1 2.734.505v2.583c-.838-.45-1.896-.703-2.734-.703"></path>
+    </svg>
+`
+
 const stripe_subscription_status_color = {
     'Email Queued': 'blue',
     'Email Sent': 'blue',
@@ -55,6 +61,136 @@ frappe.ui.form.on("Subscription", {
             ["autocharge_with_stripe","payment_gateway_account", "payment_method_configuration"].forEach(function(field) {
                 frm.set_df_property(field, "read_only", true);
             })
+
+            frappe.call({
+                method: "erpusa.stripe_plus.api.webhook_receiver_subscription.is_customer_user",
+                args: {
+                    representative: frm.doc.user_account_representative
+                },
+                callback: function (r) {
+                    if (!r.message.user) {
+                        frm.add_custom_button("Convert Customer to User", function() {
+                            frappe.call({
+                                method: "erpusa.stripe_plus.api.webhook_receiver_subscription.convert_customer_to_user",
+                                freeze: true,
+                                freeze_message: __("Converting Customer to User"),
+                                args: {
+                                    representative: frm.doc.user_account_representative,
+                                    email_address: r.message.email_address,
+                                    customer: frm.doc.party
+                                },
+                                callback: function (r) {
+                                    if (!r.message) {
+                                        frm.refresh();
+                                    }
+                                }
+                            })
+                        }, `${stripe_logo} Tools`)
+                    }
+                }
+            })
+        
+            frm.add_custom_button("Look for Unallocated Payments", function() {
+                frappe.call({
+                    method: "erpusa.stripe_plus.api.webhook_receiver_subscription.find_unallocated_payments",
+                    freeze: true,
+                    freeze_message: __("Finding Unallocated Payments"),
+                    args: {
+                        subscription_name: frm.doc.name,
+                        customer_name: frm.doc.party,
+                        stripe_subscription: frm.doc.stripe_subscription_id,
+                        payment_gateway: frm.doc.payment_gateway
+                    },
+                    callback: function (r) {
+                        if (r.message) {
+                            let unallocated_payments_dialog = new frappe.ui.Dialog({
+                                size: "large",
+                                title: "Unallocated Payments",
+                                fields: [
+                                    {
+                                        fieldname: 'instructions',
+                                        fieldtype: 'HTML',
+                                        options: `
+                                            You have <b>${r.message.invoice_count}</b> unpaid invoice(s). The following can be allocated as payments for them:
+                                        `
+                                    },
+                                    {
+                                        fieldname: 'payments',
+                                        fieldtype: 'Table',
+                                        cannot_add_rows: 1,
+                                        cannot_delete_rows: 1,
+                                        in_place_edit: 0,
+                                        data: r.message.unallocated_stripe_transactions,
+                                        fields: [
+                                            {
+                                                fieldname: 'name',
+                                                fieldtype: 'Link',
+                                                label: 'Transaction',
+                                                options: 'Stripe Transaction',
+                                                read_only: 1,
+                                                in_list_view: 1,
+                                                columns: 5
+                                            },
+                                            {
+                                                fieldname: 'payment_method_type',
+                                                fieldtype: 'Data',
+                                                label: 'Payment Method',
+                                                read_only: 1,
+                                                in_list_view: 1,
+                                                columns: 2
+                                            },
+                                            {
+                                                fieldname: 'created',
+                                                fieldtype: 'Datetime',
+                                                label: 'Received',
+                                                in_list_view: 1,
+                                                read_only: 1,
+                                                columns: 3
+                                            }
+                                        ]
+                                    }
+                                ]
+                            });
+
+                            if (r.message.unallocated_stripe_transactions.length > 0) {
+                                unallocated_payments_dialog.set_primary_action("Allocate Payments", function() {
+                                    function allocate_payments() {
+                                        frappe.call({
+                                            method: "erpusa.stripe_plus.api.webhook_receiver_subscription.allocate_payments",
+                                            freeze: true,
+                                            freeze_message: __("Allocating Payments"),
+                                            args: {
+                                                stripe_transactions: r.message.unallocated_stripe_transactions,
+                                                invoice_count: r.message.invoice_count,
+                                                payment_gateway: frm.doc.payment_gateway
+                                            },
+                                            callback: function (r) {
+                                                unallocated_payments_dialog.hide();
+                                            }
+                                        });
+                                    }
+
+                                    if (r.message.unallocated_stripe_transactions.length !== r.message.invoice_count.length) {
+                                        frappe.confirm(__("The number of unpaid invoices is not equal to the total unallocated payments. Continue?"),
+                                            () => {
+                                                allocate_payments();
+                                            },
+                                            () => {
+                                                unallocated_payments_dialog.hide();
+                                            }
+                                        )
+                                    }
+                                    else {
+                                        allocate_payments();
+                                    }
+                                });
+                            }
+
+                            unallocated_payments_dialog.show();
+                        }
+                    }
+                })
+            }, `${stripe_logo} Tools`)
 
         }
         
@@ -151,9 +287,7 @@ frappe.ui.form.on("Subscription", {
 function displayIntro(frm, stripe_subscription_status, additional_info="") {
     frm.set_intro(__(
         `<div class="d-flex align-items-center" style="gap: 0.5rem;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stripe" viewBox="0 0 16 16">
-                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.226 5.385c-.584 0-.937.164-.937.593 0 .468.607.674 1.36.93 1.228.415 2.844.963 2.851 2.993C11.5 11.868 9.924 13 7.63 13a7.7 7.7 0 0 1-3.009-.626V9.758c.926.506 2.095.88 3.01.88.617 0 1.058-.165 1.058-.671 0-.518-.658-.755-1.453-1.041C6.026 8.49 4.5 7.94 4.5 6.11 4.5 4.165 5.988 3 8.226 3a7.3 7.3 0 0 1 2.734.505v2.583c-.838-.45-1.896-.703-2.734-.703"></path>
-            </svg>
+            ${stripe_logo}
             <b>Stripe Status: ${stripe_subscription_status}</b>${additional_info? " &bull; ": ""}<span>${additional_info}</span>
         </div>`
         
--- erpusa/stripe_plus/api/webhook_receiver_subscription.py
+++ erpusa/stripe_plus/api/webhook_receiver_subscription.py
@@ -1,6 +1,7 @@
 import frappe
 from frappe import _
 import datetime
+import json
 from erpusa.stripe_plus.doctype.stripe_plus_settings.stripe_plus_settings import get_api_key_secret, get_representative_email_address, get_bank_account_for_payment_entry
 from erpnext.accounts.doctype.payment_entry.payment_entry import get_payment_entry
 
@@ -15,7 +16,7 @@ SUBSCRIPTION_STATUS_VERBOSE = {
     'paused': 'Paused'
 }
 
-def receive_stripe_subscription_events(data):
+def receive_stripe_subscription_events(data, return_invoice_id=False):
     if data.get("object") == "subscription":
         # get metadata to look for associated ERPNext subscription
         metadata = data.get("metadata")
@@ -82,13 +83,14 @@ def receive_stripe_subscription_events(data):
                     
                     customer = frappe.get_doc("Customer", frappe.db.get_value("Subscription", metadata.get("erp_subscription_name"), "party"))
                     if not frappe.db.exists("Portal User", {"parent": customer.name, "user": user.name}):
-                        customer.append("portal_user", {
+                        customer.append("portal_userss", {
                             "user": user.name
                         })
                     customer.save()
     
     # make payment entry if subscription was succesfully set up
     if data.get("object") == "invoice" and data.get("status") == "paid" and data.get("subscription"):
+        frappe.log_error("this was accessed")
         from erpusa.stripe_plus.api.webhook_receiver import create_update_stripe_transaction, get_charge_details
         
         # find the subscription associated
@@ -128,48 +130,186 @@ def receive_stripe_subscription_events(data):
                 limit=1
             )
             
-            # set the customer and associated subscription
-            mp_doc.customer = frappe.db.get_value("Subscription", subscription, "party")
-            mp_doc.associated_subscription = subscription
-            mp_doc.associated_sales_invoice = sales_invoices[0]
-            
-            ##  Create a Payment Entry for the oldest sales invoice ##
-            if not frappe.db.exists("Payment Entry Reference", { "reference_name":  sales_invoices[0]}):
-                # get the Payment Request doc and fetch the cost_center from settings
-                cost_center = frappe.db.get_single_value("Stripe Plus Settings", "merchant_fee_cost_center")
-                pe_doc = get_payment_entry("Sales Invoice", sales_invoices[0])
-                # set the actual amount paid by the user
-                for index, reference in enumerate(pe_doc.references):
-                    if reference.reference_name == sales_invoices[0]:
-                        pe_doc.references[index].allocated_amount = mp_doc.gross_amount
-                    
-                pe_doc.reference_no = frappe.db.get_value("Stripe Transaction", mp_doc.source, "payment_intent")
-                pe_doc.paid_amount = mp_doc.net_amount
-
-                # apply Merchant Payment as deduction
-                pe_doc.append("deductions", {
-                    "account": frappe.db.get_single_value("Stripe Plus Settings", "merchant_fee_account"),
-                    "cost_center": cost_center,
-                    "amount": mp_doc.merchant_fee,
-                    "description": mp_doc.name,
-                })
+            if sales_invoices:
+                # set the customer and associated subscription
+                mp_doc.customer = frappe.db.get_value("Subscription", subscription, "party")
+                mp_doc.associated_subscription = subscription
+                mp_doc.associated_sales_invoice = sales_invoices[0]
                 
-                # set the bank account
-                if get_bank_account_for_payment_entry(pe_doc.payment_type, pe_doc.paid_from, pe_doc.paid_to, False, as_dict=False):
-                    pe_doc.bank_account = get_bank_account_for_payment_entry(pe_doc.payment_type, pe_doc.paid_from, pe_doc.paid_to, False, as_dict=False)
-
-                try:
-                    pe_doc.save(ignore_permissions=True)
-                    pe_doc.submit()
-
-                except Exception as e:
-                    frappe.log_error(frappe.get_traceback(), _("Error Saving Payment Entry Document"))
+                ##  Create a Payment Entry for the oldest sales invoice ##
+                if not frappe.db.exists("Payment Entry Reference", { "reference_name":  sales_invoices[0]}):
                     
-                # update Merchant Payment doc
-                try:
-                    mp_doc.associated_payment_entry = pe_doc.name
-                    mp_doc.save()
+                    frappe.log_error("reached merchant_payment payment entry creation")
+                    # get the Payment Request doc and fetch the cost_center from settings
+                    cost_center = frappe.db.get_single_value("Stripe Plus Settings", "merchant_fee_cost_center")
+                    pe_doc = get_payment_entry("Sales Invoice", sales_invoices[0])
+                    # set the actual amount paid by the user
+                    for index, reference in enumerate(pe_doc.references):
+                        if reference.reference_name == sales_invoices[0]:
+                            pe_doc.references[index].allocated_amount = mp_doc.gross_amount
+                        
+                    pe_doc.reference_no = frappe.db.get_value("Stripe Transaction", mp_doc.source, "payment_intent")
+                    pe_doc.paid_amount = mp_doc.net_amount
 
-                except Exception as e:
-                    frappe.log_error(frappe.get_traceback(), _("Error Saving Merchant Payment Document"))
+                    # apply Merchant Payment as deduction
+                    pe_doc.append("deductions", {
+                        "account": frappe.db.get_single_value("Stripe Plus Settings", "merchant_fee_account"),
+                        "cost_center": cost_center,
+                        "amount": mp_doc.merchant_fee,
+                        "description": mp_doc.name,
+                    })
+                    frappe.log_error("reached merchant_payment payment entry created")
+                    # set the bank account
+                    if get_bank_account_for_payment_entry(pe_doc.payment_type, pe_doc.paid_from, pe_doc.paid_to, False, as_dict=False):
+                        pe_doc.bank_account = get_bank_account_for_payment_entry(pe_doc.payment_type, pe_doc.paid_from, pe_doc.paid_to, False, as_dict=False)
 
+                    try:
+                        pe_doc.save(ignore_permissions=True)
+                        pe_doc.submit()
+
+                    except Exception as e:
+                        frappe.log_error(frappe.get_traceback(), _("Error Saving Payment Entry Document"))
+                        
+                    # update Merchant Payment doc
+                    try:
+                        mp_doc.associated_payment_entry = pe_doc.name
+                        mp_doc.save()
+
+                    except Exception as e:
+                        frappe.log_error(frappe.get_traceback(), _("Error Saving Merchant Payment Document"))
+
+                        
+                    frappe.log_error("reached almost done")
+
+                if return_invoice_id:
+                    return sales_invoices[0]
+            else:
+                raise Exception("An error occured while creating an invoice.")
+
+def get_invoice_details(id, api_key):
+    import stripe
+    # get Invoice object
+    if id:
+        stripe.api_key = api_key
+
+        try:
+            invoice = stripe.Invoice.retrieve(id)
+            return invoice
+        
+        except Exception as e:
+            frappe.log_error("Error getting Invoice Details", str(e))
+            return None
+
+def list_subscription_invoices(stripe_subscription, api_key):
+    import stripe
+    # get Invoice object
+    if id:
+        stripe.api_key = api_key
+
+        try:
+            invoices = stripe.Invoice.list(
+                subscription=stripe_subscription,
+                limit=10
+            )
+            return invoices
+        
+        except Exception as e:
+            frappe.log_error("Error getting Invoice Details", str(e))
+            return None
+
+
+@frappe.whitelist()
+def is_customer_user(representative):
+  email_address = get_representative_email_address(
+    representative=representative,
+    as_dict=False,
+  )
+
+  return {
+      'user': frappe.db.exists("User", email_address),
+      'email_address': email_address
+  }
+
+@frappe.whitelist()
+def convert_customer_to_user(representative, email_address, customer):
+    user_to_authorize = frappe.db.get_single_value("Stripe Plus Settings", "user_to_authorize")
+    if user_to_authorize and email_address:
+        try:
+            frappe.set_user(user_to_authorize)
+            user = frappe.new_doc("User")
+            user.email = email_address
+            user.first_name = frappe.db.get_value("Contact", representative, "first_name")
+            user.last_name = frappe.db.get_value("Contact", representative, "last_name")
+            user.save()
+            user.append("roles", {
+                "role": "Customer"
+            })
+            user.save()
+
+            customer_doc = frappe.get_doc("Customer", customer)
+            if not frappe.db.exists("Portal User", {"parent": customer, "user": user.name}):
+                customer_doc.append("portal_users", {
+                    "user": user.name
+                })
+            customer_doc.save()
+
+        except Exception as e:
+            frappe.throw(str(e))
+
+        frappe.msgprint(_("Customer was successfully converted to a user."))
+
+@frappe.whitelist()
+def find_unallocated_payments(subscription_name, customer_name, stripe_subscription, payment_gateway):
+  api_key = get_api_key_secret(gateway_controller=frappe.db.get_value("Payment Gateway", payment_gateway, "gateway_controller"))
+  stripe_customer_id = frappe.db.get_value("Customer", customer_name, "stripe_customer_id")
+  
+  subscription_invoices = list_subscription_invoices(stripe_subscription, api_key)['data']
+  stripe_invoices = [invoice["id"] for invoice in subscription_invoices]
+  stripe_transactions = frappe.db.get_all("Stripe Transaction", fields=["name", "payment_intent", "amount", "payment_method_type", "created", "invoice"], filters={"customer": stripe_customer_id, "invoice": ["in", stripe_invoices]})
+  invoice_count = frappe.db.count("Sales Invoice", filters={"status": ["not in", ["Draft", "Paid", "Cancelled"]], "subscription": subscription_name})
+  unallocated_stripe_transactions = []
+
+  for t in stripe_transactions:
+    if not frappe.db.exists("Payment Entry", {"reference_no": t.payment_intent}):
+      unallocated_stripe_transactions.append(t)
+
+  return {
+    'unallocated_stripe_transactions': unallocated_stripe_transactions,
+    'invoice_count': invoice_count
+  }
+
+@frappe.whitelist()
+def allocate_payments(stripe_transactions, invoice_count, payment_gateway):
+  api_key = get_api_key_secret(gateway_controller=frappe.db.get_value("Payment Gateway", payment_gateway, "gateway_controller"))
+  stripe_transactions = json.loads(stripe_transactions)
+
+#   for index in range(int(invoice_count)):
+#     stripe_invoice = get_invoice_details(stripe_transactions[index]['invoice'], api_key)
+
+#     if stripe_invoice:
+#       try:
+#         sales_invoice = receive_stripe_subscription_events(stripe_invoice)
+#       except Exception as e:
+#         frappe.throw(_("Alllocation Failed"), str(e))
+
+#       frappe.msgprint(
+#         title=_("Allocation Successful"),
+#         msg=_("Stripe Transaction {} allocated for {}.").format(stripe_transactions[index]['invoice'], sales_invoice)
+#       )
+
+  for st in stripe_transactions:
+    stripe_invoice = get_invoice_details(st['invoice'], api_key)
+
+    if stripe_invoice:
+      try:
+        sales_invoice = receive_stripe_subscription_events(stripe_invoice, True)
+      except Exception as e:
+        frappe.throw(
+           title= _("Alllocation Failed"),
+           msg=str(e)
+        )
+
+      frappe.msgprint(
+        title=_("Allocation Successful"),
+        msg=_("Stripe Transaction {} allocated for {}.").format(st['invoice'], sales_invoice)
+      )
