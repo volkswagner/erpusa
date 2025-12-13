9b978b15c80011f55c177b913bf0afb407f93e2e
markguchi <rmbg2798@gmail.com>
2025-12-13 09:01:18 -0500

    Enhanced Subscription Update Request; Added Subscription Cancellation and Resubscription Request

--- erpusa/www/subscriptions.py
+++ erpusa/www/subscriptions.py
@@ -1,5 +1,6 @@
 import frappe
 from frappe import _
+from urllib.parse import urlencode, quote_plus
 from erpusa.stripe_plus.doctype.stripe_plus_settings.stripe_plus_settings import calculate_subscription_plan_total
 
 def get_context(context):
@@ -15,6 +16,15 @@ def get_context(context):
         for subscription in frappe.db.get_all("Subscription", filters={"party": context.customer}, pluck="name", order_by="status ASC"):
             subscription_data = frappe.get_doc("Subscription", subscription).as_dict()
             subscription_data["plan_list"], subscription_data["total"] = calculate_subscription_plan_total(subscription_data)
+            
+            url_params = {"subscription": subscription_data.name}
+            if subscription_data.friendly_name:
+                url_params['friendly_name'] = subscription_data.friendly_name
+                
+            subscription_data["update_request_url"] = "/subscription-update-request-form/new?" + urlencode(url_params)
+            subscription_data["cancellation_request_url"] = "/subscription-update-request-form/new?" + urlencode(url_params | {"cancellation": 1})
+            if subscription_data.status in ["Cancelled", "Completed"]:
+                subscription_data["resubscription_request_url"] = "/subscription-update-request-form/new?" + urlencode(url_params | {"resubscription": 1})
             
             context.subscriptions.append(subscription_data)
         
